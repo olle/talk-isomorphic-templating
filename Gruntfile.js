a0fd101e28f0449bf153df372a762c09d15a5dd9
Olle Törnström <toernstroem@synyx.de>
2018-10-08 09:42:49 +0200

    Remove bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,62 +1,45 @@
-module.exports = function(grunt) {
-  'use strict';
-
-  require('load-grunt-tasks')(grunt);
-
-  grunt.initConfig({
-
-    'bower-install-simple': {
-      all: {
-        // all defaults
-      },
-    },
-
-    connect: {
-      talk: {
-        options: {
-          port: 8088,
-          open: 'http://localhost:8088/talk/',
-          livereload: true,
+module.exports = function (grunt) {
+    'use strict';
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        connect: {
+            talk: {
+                options: {
+                    port: 8088,
+                    open: 'http://localhost:8088/talk/',
+                    livereload: true,
+                },
+            },
+            demo: {
+                options: {
+                    port: 8089,
+                    open: 'http://localhost:8089/src/main/webapp/index.html',
+                    livereload: 35721,
+                }
+            }
         },
-      },
-      demo: {
-        options: {
-          port: 8089,
-          open: 'http://localhost:8089/src/main/webapp/index.html',
-          livereload: 35721,
-        }
-      }
-    },
-
-    watch: {
-      talk: {
-        files: ['talk/*.html'],
-        options: {
-          livereload: true,
+        watch: {
+            talk: {
+                files: ['talk/*.html'],
+                options: {
+                    livereload: true,
+                },
+            },
+            demo: {
+                files: ['src/main/webapp/**/*'],
+                options: {
+                    livereload: 35721
+                }
+            }
         },
-      },
-      demo: {
-        files: ['src/main/webapp/**/*'],
-        options: {
-          livereload: 35721
-        }
-      }
-    },
-
-  });
-
-  grunt.registerTask('serve', [
-    'bower-install-simple',
-    'connect:talk',
-    'watch:talk'
-  ]);
-
-  grunt.registerTask('demo', [
-    'bower-install-simple',
-    'connect:demo',
-    'watch:demo'
-  ]);
-
-  grunt.registerTask('default', ['serve']);
-
+    });
+    grunt.registerTask('serve', [
+        'connect:talk',
+        'watch:talk'
+    ]);
+    grunt.registerTask('demo', [
+        'connect:demo',
+        'watch:demo'
+    ]);
+    grunt.registerTask('default', ['serve']);
 };
